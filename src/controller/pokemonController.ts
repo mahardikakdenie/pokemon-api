d9c5fe11adf4338b4ea3ceee797234a3175d457e
mahardikakdenie <kucingemas744@gmail.com>
2025-04-08 13:10:38 +0700

    create endpoint to get pokemon

---
+++ src/controller/pokemonController.ts
@@ -0,0 +1,16 @@
+import { Request as ExpressRequest, Response } from 'express';
+import { getPokemonService } from '../service/pokemonService';
+
+export const getPokemons = async (req: ExpressRequest, res: Response) => {
+  const { limit, offset } = req.query;
+    const limitNumber = Number(limit) || 10; // Default limit to 10 if not provided
+    const offsetNumber = Number(offset) || 0; // Default offset to 0 if not provided
+    let cachedPokemonList: any[] | null = null; // Cache untuk menyimpan data Pokémon
+
+    try {
+        cachedPokemonList = await getPokemonService(limitNumber, offsetNumber); // Ambil data Pokémon dari cache atau API
+       res.status(200).json(cachedPokemonList); 
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch Pokémon data' });
+    }
+}
---
+++ src/service/pokemonService.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export const getPokemonService = async (limit: number, offset: number) => {
+    let cachedPokemonList: any[] | null = null; // Cache untuk menyimpan data Pokémon
+    try {
+        // Gunakan cache jika sudah ada
+        if (cachedPokemonList) {
+            return cachedPokemonList;
+        }
+    
+        // Ambil list nama dan URL Pokémon
+        const listResponse = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=' + limit + '&offset=' + offset);
+        const pokemonList = listResponse.data.results;
+    
+        // Ambil detail masing-masing Pokémon
+        const detailedPokemonList = await Promise.all(
+          pokemonList.map(async (pokemon: any) => {
+            const detailRes = await axios.get(pokemon.url);
+            const data = detailRes.data;
+    
+            return {
+              name: data.name,
+              image: data.sprites.other['official-artwork'].front_default,
+              abilities: data.abilities.map((ab: any) => ab.ability.name),
+            };
+          })
+        );
+    
+        // Simpan ke cache
+        cachedPokemonList = detailedPokemonList;
+    
+        // res.json(detailedPokemonList);
+      } catch (err) {
+        console.error("Error fetching Pokémon data:", err);
+        throw new Error("Failed to fetch Pokémon data");
+      }
+
+      return cachedPokemonList;
+}
